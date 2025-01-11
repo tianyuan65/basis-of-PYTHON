e536963e6625bf56c29520ff73d4d3f8c56c3aba
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2025-01-11 22:46:08 +0900

    assignment operator

---
+++ 18-Python_赋值运算符.py
@@ -0,0 +1,20 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2025/1/11 22:30
+# @Author : 田园
+# @File : 18-Python_赋值运算符
+# @Project : learn_PYTHON基础
+
+# 为单个变量赋值
+a=8
+print(a)
+
+# 同时为多个变量赋值，按下面的例子，将数据23先赋值给变量c，再将以获取数据的变量c赋值给变量b。
+b=c=23
+print(b)    #23
+print(c)    #23
+
+# 多个变量赋值，用逗号进行分隔，同时给多个变量赋值不同的数据。
+d,e,f=67,'身着长衫轻摇纸扇',False
+print(d)    #67
+print(e)    #身着长衫轻摇纸扇
+print(f)    #False
