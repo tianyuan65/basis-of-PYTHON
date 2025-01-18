445b53f25b422d80fc8ce14fd796e3442c28a0cf
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2025-01-18 22:14:26 +0900

    call input function and print the input content on the console

---
+++ 24-Python_输入.py
@@ -0,0 +1,12 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2025/1/18 20:58
+# @Author : 田园
+# @File : 24-Python_输入
+# @Project : learn_PYTHON基础
+
+# 输入，input在Python中是个函数，但在前端中input是个输入框标签，就是这个<input/>
+# password=input('please enter the password.')
+# print('my password is: %s' % password)
+
+nickname=input('please enter your nickname')
+print('the nickname is: %s' % nickname)
