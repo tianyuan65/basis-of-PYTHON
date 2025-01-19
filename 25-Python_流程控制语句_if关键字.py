5c7b8a4b70ee35200a0c625e150335115a3d3bf4
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2025-01-19 22:35:07 +0900

    if decision statement

---
+++ 25-Python_流程控制语句_if关键字.py
@@ -0,0 +1,21 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2025/1/19 20:11
+# @Author : 田园
+# @File : 25-Python_流程控制语句_if关键字
+# @Project : learn_PYTHON基础
+
+# if关键字的语句结构
+# if判断条件：
+#     代码(判断条件问True时，才会执行if下面的内容)
+
+age=12
+# 若age大于18，就可以开车了
+if age>18:
+    print('你可以开车了')
+
+# True代表男性，False代表女性
+gender=True
+
+if gender==True:
+    # if下面的代码，必须是一个tab键，或是四个空格
+    print('你是个男的')
